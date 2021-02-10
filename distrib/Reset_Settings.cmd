930aed2d76857e91009314d2fa13f37c52f8f97c
v0lt <mpc-be@mail.ru>
2021-02-10 18:44:47 +0300

    Исправлен Reset_Settings.cmd.

--- distrib/Reset_Settings.cmd
+++ distrib/Reset_Settings.cmd
@@ -2,7 +2,7 @@
 echo.
 echo.
 title Restore VSFilter default settings...
-REM start /min reg delete "HKEY_CURRENT_USER\Software\MPC-BE Filters\MPC Image Source" /f
+start /min reg delete "HKEY_CURRENT_USER\Software\Gabest\VSFilter" /f
 echo    settings were reset to default
 echo.
 pause >NUL
